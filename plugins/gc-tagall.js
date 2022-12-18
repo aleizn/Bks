84dada5805d9a81741ed2a9d196d9820433ade6a
aleizn20 <virtualn641@gmail.com>
2022-12-17 23:47:54 -0500

    Propiedad De Ayle
    Team Bks

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,10 +5,10 @@ throw false
 }
 let pesan = args.join` `
 let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
+let teks = `*‼️ 𝐀𝐍𝐎𝐓𝐄𝐍𝐒𝐄 𝐌𝐑𝐃 ‼️*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
 teks += `┣➥ @${mem.id.split('@')[0]}\n`}
-teks += `*└* 𝐁𝐲 𝐍𝐨𝐭𝐉𝐡𝐮𝐳ᙆ  ᷦ\n\n*▌│█║▌║▌║║▌║▌║▌║█*`
+teks += `└𝐓𝐄𝐀𝐌 𝐁𝐊𝐒 <𝟑`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
